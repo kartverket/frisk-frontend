bd150ad1d6d8768bfc6f6266ebe20d1301dc9e0a
Christoffer Bjelke <christoffer.bjelke@outlook.com>
2024-09-09 15:07:30 +0200

    basic usage of tanstack query with the backend

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,13 +1,39 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { createLazyFileRoute } from '@tanstack/react-router'
+import { createFunction, getFunctions } from '../services/backend'
 
 export const Route = createLazyFileRoute('/')({
   component: Index,
 })
 
 function Index() {
+  const queryClient = useQueryClient()
+
+  const { data: functions } = useQuery({
+    queryKey: ['functions'],
+    queryFn: getFunctions,
+  })
+
+  const { mutate } = useMutation({
+    mutationFn: createFunction,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['functions'],
+      })
+    },
+
+  })
+
   return (
     <div className="p-2">
       <h3>Welcome Home!</h3>
+      <p>Here are your functions:</p>
+      <ul>
+        {functions?.map((f) => (
+          <li key={f.path}>{f.name}</li>
+        ))}
+      </ul>
+      <button onClick={() => mutate({ name: 'New Function' + (Math.random() * 100).toFixed(0), parentId: 1 })}>Create new function</button>
     </div>
   )
 }
---
+++ src/services/backend.ts
@@ -0,0 +1,43 @@
+const BACKEND_URL = import.meta.env.BACKEND_URL ?? "http://localhost:8080";
+const BEARER_TOKEN = import.meta.env.BEARER_TOKEN ?? "test123";
+
+// backend fetcher that appends the Bearer token to the request
+async function fetchFromBackend(url: string, options: RequestInit) {
+  const response = await fetch(url, {
+    ...options,
+    headers: {
+      ...options.headers,
+      Authorization: `Bearer ${BEARER_TOKEN}`,
+    },
+  });
+  return response;
+}
+
+export async function getFunctions() {
+  const response = await fetchFromBackend(`${BACKEND_URL}/functions`, {
+    method: "GET",
+  });
+  return await response.json();
+}
+
+export async function createFunction({
+  name,
+  parentId,
+}: {
+  name: string;
+  parentId: number;
+}) {
+  await fetchFromBackend(`${BACKEND_URL}/functions`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, parentId }),
+  });
+}
+
+export async function deleteFunction(id: number) {
+  await fetchFromBackend(`${BACKEND_URL}/functions/${id}`, {
+    method: "DELETE",
+  });
+}
