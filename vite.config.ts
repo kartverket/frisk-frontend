b5508075ea1d3d7a105ac28e7c749a61ba894482
Håkon Frøland <31542827+hakonfro@users.noreply.github.com>
2024-09-16 14:03:06 +0200

    Legger til husky, lint-staged og ignorefiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import path from "path";
+import path from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+	plugins: [TanStackRouterVite(), react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 });
