7695166529633555150b87b5ec377c2a77f2dd75
Christoffer Bjelke <christoffer.bjelke@outlook.com>
2024-09-18 14:18:45 +0200

    add authority++

--- src/services/msal.ts
+++ src/services/msal.ts
@@ -13,6 +13,14 @@ if (!clientId) {
 const configuration: Configuration = {
 	auth: {
 		clientId,
+		authority: "https://regelrettoutlook.ciamlogin.com/",
+		redirectUri: "http://localhost:5173", // Points to window.location.origin. You must register this URI on Microsoft Entra admin center/App Registration.
+		postLogoutRedirectUri: "/", // Indicates the page to navigate after logout.
+		navigateToLoginRequestUrl: false, // If "true", will navigate back to the original request location before processing the auth code response.
+	},
+	cache: {
+		cacheLocation: "localStorage", // Configures cache location. "sessionStorage" is more secure, but "localStorage" gives you SSO between tabs.
+		storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
 	},
 };
 
