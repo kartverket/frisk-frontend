36431507a29a73066e0c0fce77ed670ad8be3213
Christoffer Bjelke <christoffer.bjelke@outlook.com>
2024-09-26 14:26:06 +0200

    icons

--- src/components/function-folder-item.tsx
+++ src/components/function-folder-item.tsx
@@ -1,6 +1,6 @@
 import { Route } from "@/routes";
 import type { BackendFunction } from "@/services/backend";
-import { Card, Text } from "@kvib/react";
+import { Button, Card, Flex, Icon, Text } from "@kvib/react";
 import { Link as TSRLink } from "@tanstack/react-router";
 
 export function FunctionFolderItem({
@@ -14,18 +14,33 @@ export function FunctionFolderItem({
 				search={{ path: backendFunction.path }}
 				style={{ borderRadius: "inherit" }}
 			>
-				<Text
-					fontWeight="bold"
-					borderRadius="inherit"
-					as="span"
-					display="flex"
-					w="100%"
-					textAlign="start"
-					p={2}
+				<Flex
 					bgColor={selected ? "blue.50" : undefined}
+					borderRadius="inherit"
+					alignItems="center"
+					p={4}
 				>
-					{backendFunction.name}
-				</Text>
+					<Text
+						fontWeight="bold"
+						as="span"
+						display="flex"
+						w="100%"
+						textAlign="start"
+					>
+						{backendFunction.name}
+					</Text>
+					<Flex gap={2}>
+						<Button as={() => <Icon icon="edit" />} />
+						<Button as={() => <Icon icon="drag_indicator" />} />
+						<Button
+							as={() => (
+								<Icon
+									icon={selected ? "arrow_back_ios" : "arrow_forward_ios"}
+								/>
+							)}
+						/>
+					</Flex>
+				</Flex>
 			</TSRLink>
 		</Card>
 	);
