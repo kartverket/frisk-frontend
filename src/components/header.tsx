2fdd11ee61dc2459a6181c11484c00836653699b
Håkon Frøland <hakon.froland@gmail.com>
2024-10-01 10:57:20 +0200

    La inn design av header i henhold til skisser

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,29 +1,13 @@
-import { Flex, Header as KvibHeader, Text } from "@kvib/react";
+import { Box, Icon, Header as KvibHeader } from "@kvib/react";
 
 export function Header() {
 	return (
 		<header>
-			<WarningBanner />
-			<KvibHeader />
+			<KvibHeader
+				justifyContent="space-between"
+				showMenuButton={true}
+				showChildrenInMenu={false}
+			/>
 		</header>
-	);
-}
-
-function WarningBanner() {
-	return (
-		<Flex
-			bgColor="red.50"
-			textColor="red.500"
-			p={2}
-			alignItems="center"
-			flexDirection="column"
-		>
-			<Text>
-				Denne siden er under utvikling. Vi er ikke helt sikre på at alt funker
-				som det skal.
-			</Text>
-			<Text>Alt innhold blir liggende offentlig tilgjengelig.</Text>
-			<Text>Vær forsiktig med å legge inn sensitiv/hemmelig informasjon.</Text>
-		</Flex>
 	);
 }
