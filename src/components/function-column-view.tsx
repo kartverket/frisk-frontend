eda80a88bed42152604b0ce2a63ccfe0784bfb78
Mailn Nifeli Snieske <malin@bekk-mac-3333.shared>
2024-10-21 15:53:48 +0200

    fix layout

--- src/components/function-column-view.tsx
+++ src/components/function-column-view.tsx
@@ -1,4 +1,4 @@
-import { Flex } from "@kvib/react";
+import { Flex, Text } from "@kvib/react";
 import { FunctionColumn } from "./function-column";
 import { getIdsFromPath } from "@/lib/utils";
 
@@ -10,10 +10,15 @@ export function FunctionColumnView({ path }: FunctionColumnViewProps) {
 	const selectedFunctionIds = getIdsFromPath(path);
 
 	return (
-		<Flex w="100%" gap={2}>
-			{selectedFunctionIds?.map((id) => (
-				<FunctionColumn key={id} functionId={id} />
-			))}
+		<Flex gap="38" flexDirection="column" paddingY="38" paddingX="100">
+			<Text fontSize="2xl" fontWeight="700">
+				Funksjonsregisteret
+			</Text>
+			<Flex gap="4">
+				{selectedFunctionIds?.map((id) => (
+					<FunctionColumn key={id} functionId={id} />
+				))}
+			</Flex>
 		</Flex>
 	);
 }
