c98c62f375e7ab04b01ba1debddc0b506d83cb93
starheim98 <andreas.hernas@bekk.no>
2024-11-22 13:14:39 +0100

    added multiselect

--- src/components/metadata/metadata-input.tsx
+++ src/components/metadata/metadata-input.tsx
@@ -75,15 +75,22 @@ function SelectInput({
 		(m) => metadata.key === m.key,
 	)?.value;
 
-	const [currentMetadataValues, setCurrentMetadataValues] = useState(
+	const [currentMetadataValues, setCurrentMetadataValues] = useState<
+		SelectOption[] | undefined
+	>(
 		currentMetadata.data
 			?.filter((m) => metadata.key === m.key)
-			?.map((m) => ({ value: m.key, label: m.value })),
+			?.map((m) => ({ value: m.key, name: m.value })),
 	);
 
 	const parentMetadataValue = parentMetadata.data?.find(
 		(m) => metadata.key === m.key,
 	)?.value;
+
+	const parentMetadataValues: SelectOption[] =
+		parentMetadata.data
+			?.filter((m) => metadata.key === m.key)
+			?.map((m) => ({ value: m.key, name: m.value })) ?? [];
 
 	const options = useQuery({
 		queryKey: [metadata],
@@ -109,50 +116,13 @@ function SelectInput({
 							currentMetadataValue={currentMetadataValue}
 							parentMetadataValue={parentMetadataValue}
 						/>
-						// <Select
-						// 	name={metadata.key}
-						// 	size="sm"
-						// 	borderRadius="5px"
-						// 	placeholder={currentMetadataValue ?? metadata.placeholder}
-						// 	defaultValue={
-						// 		currentMetadataValue ??
-						// 		(metadata.inheritFromParent ? parentMetadataValue : undefined)
-						// 	}
-						// >
-						// 	{options.data?.map((option) => (
-						// 		<option key={option.value} value={option.value}>
-						// 			{option.name}
-						// 		</option>
-						// 	))}
-						// </Select>
 					) : (
-						<SearchAsync
-							id={metadata.key}
-							size="sm"
-							value={currentMetadataValues ?? undefined}
-							isMulti
-							debounceTime={100}
-							defaultOptions
-							dropdownIndicator={<Icon icon="expand_more" weight={400} />}
-							loadOptions={(inputValue, callback) => {
-								const filteredOptions = options.data
-									?.filter((option) =>
-										option.name
-											.toLowerCase()
-											.includes(inputValue.toLowerCase()),
-									)
-									.map((option) => ({
-										value: option.value,
-										label: option.name,
-									}));
-								// @ts-expect-error
-								callback(filteredOptions);
-							}}
-							onChange={(newValue) => {
-								// @ts-expect-error
-								setCurrentMetadataValues(newValue ?? []);
-							}}
-							placeholder="Søk"
+						<MultiSelect
+							options={options}
+							metadata={metadata}
+							currentMetadataValues={currentMetadataValues}
+							parentMetadataValues={parentMetadataValues}
+							setCurrentMetadataValues={setCurrentMetadataValues}
 						/>
 					)
 				) : options.isError ? (
@@ -193,6 +163,54 @@ function SingleSelect({
 				</option>
 			))}
 		</Select>
+	);
+}
+
+type MultiSelectProps = {
+	metadata: SelectMetadata;
+	options: UseQueryResult<SelectOption[]>;
+	currentMetadataValues: SelectOption[] | undefined;
+	parentMetadataValues: SelectOption[] | undefined;
+	setCurrentMetadataValues: (values: SelectOption[]) => void;
+};
+
+function MultiSelect({
+	metadata,
+	options,
+	currentMetadataValues,
+	parentMetadataValues,
+	setCurrentMetadataValues,
+}: MultiSelectProps) {
+	return (
+		<SearchAsync
+			id={metadata.key}
+			size="sm"
+			value={
+				currentMetadataValues ??
+				(metadata.inheritFromParent ? parentMetadataValues : undefined)
+			}
+			isMulti
+			debounceTime={100}
+			defaultOptions
+			dropdownIndicator={<Icon icon="expand_more" weight={400} />}
+			loadOptions={(inputValue, callback) => {
+				const filteredOptions = options.data
+					?.filter((option) =>
+						option.name.toLowerCase().includes(inputValue.toLowerCase()),
+					)
+					.map((option) => ({
+						value: option.value,
+						label: option.name,
+					}));
+				// @ts-expect-error
+				callback(filteredOptions);
+			}}
+			onChange={(newValue) => {
+				// @ts-expect-error
+				setCurrentMetadataValues(newValue ?? []);
+			}}
+			placeholder="Søk"
+		/>
 	);
 }
 
