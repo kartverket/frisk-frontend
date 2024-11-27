77f19d1fd93f3bd1903f65e728a30d9f1ad946c0
starheim98 <andreas.hernas@bekk.no>
2024-11-27 13:02:46 +0100

    fix

--- frisk.config.ts
+++ frisk.config.ts
@@ -18,7 +18,7 @@ export const config: FriskConfig = {
 				const team = await getTeam(input.value);
 				return team.displayName;
 			},
-			selectMode: "multi",
+			selectMode: "single",
 			showOn: "createAndUpdate",
 			isRequired: true,
 			placeholder: "Velg team",
