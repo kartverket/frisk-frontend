60298dd1539493d2bad108aa21f8d87741cd112d
starheim98 <andreas.hernas@bekk.no>
2025-01-08 11:20:01 +0100

    added functionality for creating RR context from FRISK

--- frisk.config.ts
+++ frisk.config.ts
@@ -6,6 +6,9 @@ import {
 	getTeam,
 } from "@/services/backend";
 import { getregelrettFrontendUrl } from "@/config";
+import { msalInstance } from "@/services/msal";
+import { InteractionRequiredAuthError } from "@azure/msal-browser";
+import { object, string } from "zod";
 
 export const config: FriskConfig = {
 	metadata: [
@@ -220,3 +223,80 @@ type Logo = {
 	imageSource: string;
 	logoLink?: string;
 };
+
+async function getRegelrettTokens() {
+	const regelrettScopes = [
+		"api://e9dc946b-6fef-44ab-82f1-c0ec2e402903/.default",
+	];
+
+	const accounts = msalInstance.getAllAccounts();
+	const account = accounts[0];
+	if (!account) {
+		throw new Error("No active account");
+	}
+	const tokenResponse = await msalInstance
+		.acquireTokenSilent({
+			scopes: regelrettScopes,
+			account: account,
+		})
+		.catch((error) => {
+			if (error instanceof InteractionRequiredAuthError) {
+				return msalInstance.acquireTokenRedirect({
+					scopes: regelrettScopes,
+					account: account,
+				});
+			}
+		});
+
+	if (!tokenResponse) {
+		throw new Error("No tokenResponse");
+	}
+	return tokenResponse;
+}
+
+async function fetchFromRegelrett(path: string, options: RequestInit = {}) {
+	const REGELRETT_URL =
+		// "https://regelrett-frontend-1024826672490.europe-north1.run.app/api/";
+		"http://localhost:8080/";
+
+	const tokens = await getRegelrettTokens();
+	const response = await fetch(`${REGELRETT_URL}${path}`, {
+		...options,
+		headers: {
+			...options.headers,
+			Authorization: `Bearer ${tokens.accessToken}`,
+		},
+	});
+	if (!response.ok) {
+		throw new Error(`Backend error: ${response.status} ${response.statusText}`);
+	}
+	return response;
+}
+
+export async function createRegelrettContext(
+	name: string,
+	teamId: string,
+	tableId: string,
+) {
+	const response = await fetchFromRegelrett("/contexts", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			name,
+			teamId,
+			tableId,
+		}),
+	});
+
+	const json = await response.json();
+	return RegelrettContext.parse(json);
+}
+
+const RegelrettContext = object({
+	id: string(),
+	name: string(),
+	tableId: string(),
+	teamId: string(),
+});
