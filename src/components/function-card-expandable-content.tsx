f3052f2e0128b6372a35cd81290348e10accaed5
malinnsnieske <malin.snieske@bekk.no>
2025-01-17 12:49:54 +0100

    separate expanded and selected card (#220)
    Co-authored-by: Mailn Nifeli Snieske <malin@bekk-mac-3333.shared>

--- src/components/function-card-expandable-content.tsx
+++ src/components/function-card-expandable-content.tsx
@@ -1,12 +1,11 @@
-import { Flex, Text, Skeleton, Stack } from "@kvib/react";
 import { useFunction } from "@/hooks/use-function";
-import { EditAndSelectButtons } from "./edit-and-select-buttons";
 import { MetadataView } from "./metadata/metadata-view";
 import { useMetadata } from "@/hooks/use-metadata";
 import { Route } from "@/routes";
 import { OboFlowFeature } from "../../frisk.config";
+import { Stack } from "@kvib/react";
 
-export function FunctionCardSelectedView({
+export function FunctionCardExpandableContent({
 	functionId,
 }: { functionId: number }) {
 	const { func } = useFunction(functionId);
@@ -15,21 +14,7 @@ export function FunctionCardSelectedView({
 	const { flags } = Route.useSearch();
 
 	return (
-		<Stack paddingLeft="10px" w="100%">
-			<Flex alignItems="center" w="100%" flex-wrap="wrap">
-				<Skeleton isLoaded={!func.isLoading} flex={1} minWidth={0}>
-					<Text
-						fontWeight="bold"
-						as="span"
-						display="flex"
-						w="100%"
-						overflow="hidden"
-					>
-						{func.data?.name ?? "<Det skjedde en feil>"}
-					</Text>
-				</Skeleton>
-				<EditAndSelectButtons functionId={functionId} selected />
-			</Flex>
+		<Stack pl="10px">
 			{config.metadata?.map((meta) => (
 				<MetadataView key={meta.key} metadata={meta} functionId={functionId} />
 			))}
