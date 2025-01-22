a9db23429f00f175928078a07599b680b1fd186b
starheim98 <andreas.hernas@bekk.no>
2025-01-22 14:51:00 +0100

    correct regelrett api scopes for SKIP

--- nginx/main.js
+++ nginx/main.js
@@ -5,7 +5,7 @@ function getConfig(r) {
 		redirect_uri: process.env.VITE_LOGIN_REDIRECT_URI,
 		backend_url: process.env.VITE_BACKEND_URL,
 		regelrett_frontend_url: process.env.VITE_REGELRETT_FRONTEND_URL,
-		regelrett_client_id: process.env.VITE_REGELRETT_CLIENT_ID,
+		regelrett_client_id: process.env.REGELRETT_CLIENT_ID,
 	};
 	r.headersOut["Content-Type"] = "application/json";
 	r.return(200, JSON.stringify(config));
