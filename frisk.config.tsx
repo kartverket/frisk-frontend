0865c6d2054bb1be26930d73cabc142389cf690c
Mailn Nifeli Snieske <malin@bekk-mac-3333.shared>
2025-01-30 16:23:04 +0100

    fix

--- frisk.config.tsx
+++ frisk.config.tsx
@@ -142,7 +142,7 @@ export async function getConfig(): Promise<FriskConfig> {
 						});
 						const url = `${getregelrettFrontendUrl()}/context/${contextId}?${searchParams.toString()}`;
 						return {
-							displayValue: "Skjema",
+							displayValue: schema.name,
 							displayOptions: {
 								type: "custom",
 								component: (
@@ -403,7 +403,7 @@ function SchemaDisplay({
 				onClick={(e) => e.stopPropagation()}
 				overflow="hidden"
 				fontWeight="medium"
-				fontSize="sm"
+				fontSize="xs"
 				justifyContent="start"
 				flex="1"
 			>
