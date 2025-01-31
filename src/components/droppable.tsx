1d4d9707d210bcc5c48c98ae6af42db221e9b1fb
JegHeterGyda <100707649+JegHeterGyda@users.noreply.github.com>
2025-01-31 08:50:23 +0100

    drop zone på funksjonskort (#270)
    * made select button droppable
    
    * added navigation when droped inside closed card

--- src/components/droppable.tsx
+++ src/components/droppable.tsx
@@ -1,49 +1,23 @@
-import { useDndMonitor, useDroppable } from "@dnd-kit/core";
-import { Box } from "@kvib/react";
-import { useState, type ReactNode } from "react";
-import { useFunction } from "@/hooks/use-function";
+import { useDroppable } from "@dnd-kit/core";
 
 type DroppableProps = {
-	id: number;
-	children: ReactNode;
+	groupId: number;
+	droppableId: string;
+	children: (props: {
+		isOver: boolean;
+		setNodeRef: (element: HTMLElement | null) => void;
+	}) => React.ReactNode;
 };
 
-export function Droppable({ id, children }: DroppableProps) {
-	const { func } = useFunction(id);
-
-	const [disabled, setDisabled] = useState<boolean>(false);
-
-	const { isOver, setNodeRef } = useDroppable({
-		id: id,
-		disabled: disabled,
-	});
-
-	useDndMonitor({
-		onDragOver(event) {
-			const { active, over } = event;
-			if (func?.data && over && active.data.current) {
-				if (over.id === id && active.data.current.func.parentId === over.id) {
-					setDisabled(true);
-				} else if (over.id === id) {
-					setDisabled(func.data.path.includes(active.id.toString()));
-				}
-			}
-		},
-		onDragEnd() {
-			setDisabled(false);
+export function Droppable({ groupId, droppableId, children }: DroppableProps) {
+	const { setNodeRef, over } = useDroppable({
+		id: droppableId,
+		data: {
+			group: groupId,
 		},
 	});
 
-	return (
-		<Box
-			ref={setNodeRef}
-			padding={"8px"}
-			backgroundColor={isOver ? "blue.100" : "gray.200"}
-			borderRadius="md"
-			border="1px"
-			marginBottom={2}
-		>
-			{children}
-		</Box>
-	);
+	const isOver = over?.data.current?.group === groupId;
+
+	return children({ isOver, setNodeRef });
 }
